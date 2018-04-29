d785f8e927c0922a9dc62db0c51ee97236c04ffe
liyayong <fengyun_yi_pure@163.com>
2018-04-29 08:11:57 +0800

    添加配置替换检查脚本

--- smartcnfchk.py
+++ smartcnfchk.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python2
 # coding=utf-8
 '''
-修改配置文件，xml, propertirs,
+根据smartcnf.ini 检查文件是否已经进行替换
+usage: python smartcnfchk.py smartcnf.ini
 '''
 import ConfigParser as parser
 import os
@@ -10,12 +11,23 @@ import time
 import shutil
 import platform
 import logging
+import sys
+from string import Template
 
 # 配置文件
 config_file='smartcnf.ini'
 midify_time=time.strftime("%Y%m%d%H%M%S", time.localtime())
 # 主机类型
 os_type=platform.system()
+
+key_value_split='='
+origin="${origin}"
+
+# report info
+report_head=Template('#---------[$file_name] modify check report start--------')
+report_ok_msg = Template("OK: [$key = $value] in [$file_name]")
+report_error_msg = Template("ERROR: [$key = $value] in [$file_name],should be modify to [${s_value}]")
+report_tail=Template('#---------[$file_name] modify check report ending--------')
 
 
 def cmd_run(cmd):
@@ -28,14 +40,16 @@ def cmd_run(cmd):
     return ans.readlines()
 
 class PropertiesFileParse(object):
-
+    '''
+        解析配置文件
+    '''
     def __init__(self,file_name):
         self._file_name = file_name
 
     def read_file(self):
         if os.path.isfile(self._file_name):
             logger.error("{} is not a file",self._file_name)
-            raise FileExistsError(self._file_name+" is not exist")
+            raise IOError (self._file_name+" is not exist")
         else:
             with open(self._file_name,'r') as f_in:
                 contents = f_in.readlines()
@@ -77,7 +91,6 @@ def getConfigInfo():
         # 校验主机与应用名是否符合要求
         if len(splits) == 3 and ((host_name not in section and splits[1] != "all") or (app_name not in section and splits[2]!="all")):
             continue
-
 
         model = splits[0].strip()
         values = config.items(section)
@@ -141,9 +154,9 @@ def convert_file_sep(file_path):
         return file_path
     fi
 
-def modify_config_file(modify_info):
+def check_config_file(modify_info):
     '''
-    根据给定的配置文件进行文件备份与修改
+    根据给定的配置文件进行文件检查与备份
     :param modify_info: data of dict
     :return: none
     '''
@@ -153,52 +166,89 @@ def modify_config_file(modify_info):
         logger.debug(file_name_deal)
 
         modifies= modify_info[file_name]
-        try:
-            backup_file_name=backup_file(file_name_deal)
-        except IOError as msg:
-            print(msg)
-            continue
-
         if os.path.basename(file_name_deal).endswith('xml'):
-           return
+            print ("now can't check xml file, file name is "+file_name_deal)
+            return
         else:
-            # with open(backup_file_name, 'rU') as f_in:
-            with open(backup_file_name,'r') as f_in:
-                #print(f_in.readlines())
-                key_used=[]
-                with open(workspce+os.sep+file_name,'w') as f_out:
-                    lines  = f_in.readlines()
-                    for line in lines:
-                        # 替换掉换行符
-                        line = line.replace('\r\n','').replace('\n','').replace('\r','')
+            # 读取文件内容
+            try:
+                file_contents = get_file_content(file_name_deal)
+            except IOError as e:
+                logger.error(e)
+                continue
+            full_file_path = os.getcwd() + os.sep +file_name_deal
+            file_base_name=os.path.basename(file_name_deal)
 
-                        if line.startswith("#") or not len(line):
-                            f_out.write(line+'\n')
-                        elif line.split('=')[0].strip() in modifies.keys():
-                            key=line.split('=')[0].strip()
-                            value=modifies[key]
-                            # 在原先值的后面进行新增
-                            if value.startswith("${origin}"):
-                                value = line.split('=')[1].strip()
-                                value = modifies[key].replace("${origin}",value)
+            # 打印报告首部
+            print report_head.substitute(file_name=full_file_path)
+            defule_value = 'is not set'
+            report_contents=[]
+            # 检查配置项是否进行了替换
+            for key,value in modifies.items():
+                prod_value = file_contents.get(key,defule_value)
 
-                            # 替换key的值
-                            f_out.write(key+"="+value+'\n')
-                            key_used.append(key)
+                if value == prod_value:
+                    msg = report_ok_msg.substitute(key=key,value=prod_value,file_name=file_base_name)
+                else:
+                    if value.startswith(origin):
+                        suffix = value.replace(origin,'')
+                        if file_contents.get(key,'').endswith(suffix):
+                            msg = report_ok_msg.substitute(key=key, value=prod_value, file_name=file_base_name)
                         else:
-                            f_out.write(line + '\n')
+                            msg = report_error_msg.substitute(key=key,value=prod_value,file_name=file_base_name,s_value=value)
+                    else:
+                        msg = report_error_msg.substitute(key=key, value=prod_value, file_name=file_base_name,s_value=value)
+                report_contents.append(msg)
 
-                    # 新增 key-value 值
-                    for key in modifies.keys():
-                        if key not in key_used:
-                            print(key+" is not set in "+file_name)
-                            #f_out.write(key+"="+modifies[key]+'\n')
+            msgs = [x for x in report_contents if x.startswith("OK")]
+            msgs.extend([x for x in report_contents if x.startswith("ERROR")])
+
+            # 打印报告内容
+            for info in msgs:
+                print(info)
+
+            # 打印报告尾部内容
+            print report_tail.substitute(file_name=full_file_path)
+            print("")
+
+def get_file_content(file_name):
+    '''
+    根据文件名进行文件读取，并丢掉注释与空行
+    :param file_name:
+    :return:
+    '''
+    if os.path.exists(file_name):
+        with open(file_name,'r') as f_in:
+            contents = f_in.readlines()
+            logger.debug(contents)
+            # 处理文件换行符
+            contents = [x.replace('\r\n','').replace('\n','').replace('\r','') for x in contents]
+            #  删除注释与空行
+            contents = [x for x in contents if not x.startswith("#")]
+            contents = [x  for x in contents if len(x) != 0]
+            logger.debug(contents)
+
+            content_dict = {}
+            for content in contents:
+                key  = content.split(key_value_split)[0].strip()
+                value  = content.split(key_value_split)[1].strip()
+                content_dict[key] = value
+            return content_dict
+    else:
+        raise IOError(file_name+" is didn't exist")
+
 
 if __name__ == '__main__':
     # 主机名
     host_name = socket.gethostname()
     workspce = os.getcwd()
     app_name = os.path.basename(workspce)
+    args = sys.argv
+    # print(args)
+    if len(args) < 2:
+        print("Usage: python smartcnfchk.py smartcnf.ini")
+        SystemExit(0)
+    config_file=args[1]
 
     logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(__name__)
@@ -206,5 +256,6 @@ if __name__ == '__main__':
     config_info=getConfigInfo()
     convert(config_info)
     logger.debug(config_info)
-    modify_config_file(config_info)
-    print('config files have been modifyed!')
+
+    check_config_file(config_info)
+    print('config files have been checked!')
