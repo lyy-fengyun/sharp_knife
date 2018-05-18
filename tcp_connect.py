561801edb40ec7a79ca2a50bf4a131087d9469b2
liyayong <fengyun_yi_pure@163.com>
2018-05-18 22:02:26 +0800

    添加测试端口是否连通的python脚本

---
+++ tcp_connect.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import socket
+import sys
+sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sk.settimeout(2)
+if len(sys.argv) <3:
+    print "Usage: ./tcp_connect.py dest_ip port"
+    sys.exit(20)
+
+green_start='\033[1;32m'
+red_start='\033[1;31m'
+color_closed='\033[0m'
+
+dest_ip,port = sys.argv[1:]
+
+def get_host_ip():
+    '''
+    获取本机ip
+    :return:
+    '''
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    except Exception as e:
+        ip = socket.gethostbyname(socket.gethostname())
+    finally:
+        s.close()
+    return ip
+
+local_ip = get_host_ip()
+try:
+  sk.connect((dest_ip,port))
+  print '{} From {} telnet to {} on port {} OK!{}'.format(green_start,local_ip,dest_ip,port,color_closed)
+except Exception:
+    print '{} From {} telnet to {} on port {} Failed!{}'.format(red_start,local_ip,dest_ip, port,color_closed)
+sk.close()
