aa38d60705b94b9abfe7361976a0a8f17cb5228b
liyayong <fengyun_yi_pure@163.com>
2018-07-01 22:46:03 +0800

    添加告警统计函数

--- count.py
+++ count.py
@@ -5,33 +5,47 @@ Spyder Editor
 This is a temporary script file.
 """
 import pandas as pd 
-import numpy as np 
+import numpy as np
 
-file_name='file:///C:/Users/liyayong/Desktop/告警日报模板/告警日志导出/20180624-sampleExport.xls'
-msgs = pd.read_excel(file_name)
 
-sys_list = set(msgs["处理意见"])
-needed_value=[]
-for info in sys_list:
-    if info.startswith("严重告警"):
-        needed_value.append(info)
+class AnalysisAlarmData(object):
 
-print(needed_value)
-#print(sys_list)
+    def __init__(self, data_file):
+        '''
+         constructor of AnalysisAlarmData
+        :param data_file: excel file name, which stored alarm data
+        '''
+        if  not isinstance(data_file,str):
+            raise TypeError("参数传递错误")
+        self.ori_data = pd.read_excel(data_file)
 
-# 1. 筛选出严重告警,电话保障的告警
-deal_msg =  msgs[msgs["处理意见"].isin(needed_value)]
+    def count_dealed_alarm(self):
+        '''
+        statistic alarm data according to province,business line,alarm content
+        :return: None
+        '''
+        # 1. 筛选出严重告警,电话保障的告警
+        sys_list = set(self.ori_data["处理意见"])
+        needed_value = []
+        for info in sys_list:
+            if info.startswith("严重告警"):
+                needed_value.append(info)
+        deal_msg = self.ori_data[self.ori_data["处理意见"].isin(needed_value)]
 
-# 2. 重新排序
-deal_msg = deal_msg.reindex(range(0,len(deal_msg.index)))
+        # 2. 重新排序
+        deal_msg = deal_msg.reindex(range(0, len(deal_msg.index)))
 
-# 3. 根据业务线，省份，告警内容进行初步统计
-ans = deal_msg.groupby(['业务线','省份','告警内容']).size()
+        # 3. 根据业务线，省份，告警内容进行初步统计
+        ans = deal_msg.groupby(['省份', '业务线', '告警内容']).size().sort_values()
 
-# 4. 对排序结果进行类型转换
-ans = ans.to_frame()
+        # 4. 对排序结果进行类型转换
+        ans = ans.to_frame()
 
-# 将结果写入到excel中
-ans.to_excel('C:/Users/liyayong/Desktop/ans.xls','sheet1')
+        # 将结果写入到excel中
+        ans.to_excel('C:/Users/liyayong/Desktop/ans.xls', 'sheet1')
 
-print(ans.head)     
+
+if __name__ == "__main__":
+    file_name = 'file:///C:/Users/liyayong/Desktop/告警日报模板/告警日志导出/20180624-sampleExport.xls'
+    analysis = AnalysisAlarmData(file_name)
+    analysis.count_dealed_alarm()
--- parseExcel.py
+++ parseExcel.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
 # coding=utf-8
 
+import xlrd
+
+class ExcelFileReader(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file_reader = xlrd.parse(file_name)
+
+    def getSheetName(self):
+        pass
+
+    def getSheet(self,sheet_name):
+        pass
+
+    def getTitleFromSheet(self):
+        pass
+
+    def getContentFromSheet(self):
+        pass
 
 if __name__ == '__main__':
+
     pass
